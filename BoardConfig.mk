74d1c3f5e3dfa94f2383a4399f30e7ed53ad5e6f
Divkix <23524935+Divkix@users.noreply.github.com>
2021-04-06 11:20:47 +0530

    SHRP Bringup

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -124,3 +124,30 @@ TW_INCLUDE_FUSE_EXFAT := true
 
 # NTFS Support
 TW_INCLUDE_FUSE_NTFS := true
+
+
+#Ignore Missing Dependencies
+ALLOW_MISSING_DEPENDENCIES=true
+
+#SHRP_Variables
+SHRP_PATH := $(LOCAL_PATH)
+SHRP_MAINTAINER := Divkix
+SHRP_DEVICE_CODE := tulip
+SHRP_EDL_MODE := 1
+SHRP_EXTERNAL := /external_sd
+SHRP_INTERNAL := /sdcard
+SHRP_OTG := /usbotg
+SHRP_REC := /dev/block/bootdevice/by-name/recovery
+SHRP_REC_TYPE := Treble
+SHRP_DEVICE_TYPE := A-Only
+SHRP_FLASH := 1
+
+# SHRP Padding Flag (For round corners)
+SHRP_STATUSBAR_RIGHT_PADDING := 40
+SHRP_STATUSBAR_LEFT_PADDING := 40
+
+# SHRP Express, enables on-the-fly theme patching (also persistent) + persistent lock
+SHRP_EXPRESS := true
+
+# Making device official
+SHRP_OFFICIAL := true
